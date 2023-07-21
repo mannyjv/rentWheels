20b6a22148fe7132f6686936492054b0e43faca1
mannyjv <emmanuel.vazquez06@gmail.com>
2023-07-21 14:54:28 +0200

    feature: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className={"relative"}>
         <NavBar />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
