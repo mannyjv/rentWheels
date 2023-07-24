2a81306154fcb9d1caaf0542220a1a5b47288fc6
mannyjv <emmanuel.vazquez06@gmail.com>
2023-07-24 15:14:50 +0200

    add hero button logic to scroll to discover section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,14 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  const handleScroll = () => {};
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
